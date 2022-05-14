a2ee755eb346b9e78e7d609282c4db9feeb920a8
codeftshaunak <sagardeyyash19@gmail.com>
2022-05-14 11:26:05 +0600

    shop is set

--- src/components/public/Pages/Shop/Shop.js
+++ src/components/public/Pages/Shop/Shop.js
@@ -1,25 +1,25 @@
-// import React, { useEffect, useState } from "react";
-// import setBookItem from "../../../hooks/setBookItem";
-// import Bookshop from "../Bookshop/Bookshop";
-// import ManageProduct from "../ManageProduct/ManageProduct";
-// import "./Shop.css";
+import React, { useEffect, useState } from "react";
+import setBookItem from "../../../hooks/setBookItem";
+import Bookshop from "../Bookshop/Bookshop";
+import ManageProduct from "../ManageProduct/ManageProduct";
+import "./Shop.css";
 
-// function Shop() {
-//   const [booksitem] = setBookItem();
-//   const booksItem = booksitem.slice(0, 6);
+function Shop() {
+  const [booksitem] = setBookItem();
+  const booksItem = booksitem.slice(0, 6);
 
-//   return (
-//     <>
-//       <div className="books__shop">
-//         <h1>Smaple Products</h1>
-//       </div>
-//       <div className="books__details">
-//         {booksItem.map((books) => (
-//           <Bookshop key={books._id} books={books}></Bookshop>
-//         ))}
-//       </div>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <div className="books__shop">
+        <h1>Smaple Products</h1>
+      </div>
+      <div className="books__details">
+        {booksItem.map((books) => (
+          <Bookshop key={books._id} books={books}></Bookshop>
+        ))}
+      </div>
+    </>
+  );
+}
 
-// export default Shop;
+export default Shop;
