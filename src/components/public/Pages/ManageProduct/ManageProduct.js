931549a8022576ae08fee98140421d3f3d5bb17d
codeftshaunak <sagardeyyash19@gmail.com>
2022-05-14 11:29:23 +0600

    Manage Product

--- src/components/public/Pages/ManageProduct/ManageProduct.js
+++ src/components/public/Pages/ManageProduct/ManageProduct.js
@@ -1,22 +1,22 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import setBookItem from "../../../hooks/setBookItem";
-// import ManageProducts from "../ManageProducts/ManageProducts";
-// import "./ManageProduct.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import setBookItem from "../../../hooks/setBookItem";
+import ManageProducts from "../ManageProducts/ManageProducts";
+import "./ManageProduct.css";
 
-// function ManageProduct() {
-//   const [booksItem] = setBookItem();
-//   return (
-//     <>
-//       <h2>Manage All Products</h2>
-//       <div className="manage__prod">
-//         {booksItem.map((books) => (
-//           <ManageProducts key={books._id} books={books}></ManageProducts>
-//         ))}
-//       </div>
-//       <Link to="/additems"> Add Items </Link>
-//     </>
-//   );
-// }
+function ManageProduct() {
+  const [booksItem] = setBookItem();
+  return (
+    <>
+      <h2>Manage All Products</h2>
+      <div className="manage__prod">
+        {booksItem.map((books) => (
+          <ManageProducts key={books._id} books={books}></ManageProducts>
+        ))}
+      </div>
+      <Link to="/additems"> Add Items </Link>
+    </>
+  );
+}
 
-// export default ManageProduct;
+export default ManageProduct;
