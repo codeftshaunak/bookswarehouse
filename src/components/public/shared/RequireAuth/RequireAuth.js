75d86eaff7440660a11aef5cf53fe9bb473bc247
codeftshaunak <sagardeyyash19@gmail.com>
2022-05-14 11:23:27 +0600

    code require auth done

--- src/components/public/shared/RequireAuth/RequireAuth.js
+++ src/components/public/shared/RequireAuth/RequireAuth.js
@@ -1,18 +1,18 @@
-// import React from "react";
-// import { useAuthState } from "react-firebase-hooks/auth";
-// import { Navigate, useLocation } from "react-router-dom";
-// import app from "../../../Firebase/firebase.init";
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Navigate, useLocation } from "react-router-dom";
+import app from "../../../Firebase/firebase.init";
 
-// function RequireAuth({ children }) {
-//   const [users, loading] = useAuthState(app);
-//   const location = useLocation();
-//   if (loading) {
-//     return <h1>Loading...</h1>;
-//   }
-//   if (!users) {
-//     return <Navigate to="/login" state={{ from: location }} replace />;
-//   }
-//   return children;
-// }
+function RequireAuth({ children }) {
+  const [users, loading] = useAuthState(app);
+  const location = useLocation();
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (!users) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+  return children;
+}
 
-// export default RequireAuth;
+export default RequireAuth;
